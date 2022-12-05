cf31b9cd5899215034bda6edbc0c12efcd0e2e94
Andrew <kaioshin2005@yahoo.com>
2022-12-05 14:51:21 -0600

    Fixed top link animation on reload

--- static/js/top.js
+++ static/js/top.js
@@ -5,51 +5,64 @@
 
 const scroll_speed = 10;
 
-function top_link() {
+function top_link(anim = true) {
     if (window.scrollY == 0) {
         hide_top_link();
     } else {
-        show_top_link();
+        show_top_link(anim);
     }
 }
 
 function hide_top_link() {
     var link = document.getElementById('top-link');
+
     if (link) {
         if (link.classList.contains('shown')) {
             link.classList.remove('shown');
         }
+
+        if (link.classList.contains('show_anim')) {
+            link.classList.remove('show_anim');
+        }
     }
 }
 
-function show_top_link() {
+function show_top_link(anim) {
     var link = document.getElementById('top-link');
-    if (link && link.style.display != "inline") {
-        if (!link.classList.contains('shown')) {
-            link.classList.add('shown');
+
+    if (link && !link.classList.contains('shown')) {
+        link.classList.add('shown');
+
+        if (anim && !link.classList.contains('show_anim')) {
+            link.classList.add('show_anim');
         }
     }
 }
 
 function smoothscroll_top() {
     var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+
     if (currentScroll > 0) {
         window.requestAnimationFrame(smoothscroll_top);
         window.scrollTo(0, currentScroll - (currentScroll / scroll_speed));
     }
 }
 
-window.addEventListener("scroll", top_link, false);
-
 addEventListener('DOMContentLoaded', (e) => {
+    top_link(false);
+
     var link = document.getElementById('top-link');
+
     if (link) {
         link.addEventListener('click', function buttonClicked(e) {
             e.preventDefault();
             smoothscroll_top();
         });
+
         link.addEventListener('transitionend', function () {
             link.classList.remove('top-link-transition');
         }, false);
+
+        window.addEventListener("scroll", top_link, false);
     }
 }, false);
